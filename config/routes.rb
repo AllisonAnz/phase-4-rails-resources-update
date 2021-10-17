2c841b8372cdbb2553998e7016cfa7d79ee59242
AllisonAnz <AllisonAnzalone@gmail.com>
2021-10-17 12:04:55 -0500

    Completed Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+        # the endpoint        # the name of the controller action
+  patch "birds/:id/like", to: "birds#increment_likes"
 end
